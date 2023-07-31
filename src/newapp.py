560f321f88840c4051562e4369518c0ee37d61eb
TmnSyk <78849961+IHW-TS@users.noreply.github.com>
2023-07-31 14:46:14 +0200

    Update newapp.py

--- src/newapp.py
+++ src/newapp.py
@@ -1,8 +1,36 @@
-from datetime import *
-from dateutil.relativedelta import *
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
+def add_time(now, intervals):
+    for interval, amount in intervals.items():
+        if interval == 'years':
+            now = now + relativedelta(years=amount)
+        elif interval == 'months':
+            now = now + relativedelta(months=amount)
+        elif interval == 'weeks':
+            now = now + relativedelta(weeks=amount)
+        elif interval == 'days':
+            now = now + relativedelta(days=amount)
+        elif interval == 'hours':
+            now = now + relativedelta(hours=amount)
+        elif interval == 'minutes':
+            now = now + relativedelta(minutes=amount)
+        elif interval == 'seconds':
+            now = now + relativedelta(seconds=amount)
+        else:
+            raise ValueError("Interval invalide")
+    return now
+
+# Obtention de la date et de l'heure actuelles
 now = datetime.now()
-print(now)
+print("Date et heure actuelles:", now)
 
-now = now + relativedelta(months=1, weeks=1, hour=10)
+# Ajout de différents intervalles de temps à la date et l'heure actuelles
+intervals = {
+    'months': 1,
+    'weeks': 1,
+    'hours': 10
+}
+now = add_time(now, intervals)
 
-print(now)
+print("Date et heure mises à jour:", now)
